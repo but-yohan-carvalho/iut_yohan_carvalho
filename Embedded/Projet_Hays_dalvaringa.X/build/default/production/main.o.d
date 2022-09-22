0ce5ac904132df4993285614d7a02753ecaeab03
Robot <touloniut@gmail.com>
2022-09-22 07:59:43 +0200

    adc 2

---
+++ Embedded/Projet_Hays_dalvaringa.X/build/default/production/main.o.d
@@ -0,0 +1,12 @@
+build/default/production/main.o: main.c ChipConfig.h IO.h timer.h PWM.h \
+ ADC.h
+
+ChipConfig.h:
+
+IO.h:
+
+timer.h:
+
+PWM.h:
+
+ADC.h:
---
+++ Embedded/Projet_Hays_dalvaringa.X/newmainXC16.c
@@ -0,0 +1,13 @@
+/*
+ * File:   newmainXC16.c
+ * Author: TP-EO-1
+ *
+ * Created on 21 septembre 2022, 14:53
+ */
+
+
+#include "xc.h"
+
+int main(void) {
+    return 0;
+}
